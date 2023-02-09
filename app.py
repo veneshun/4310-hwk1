871bf79a36196e580d196985940908e08b4021c0
Victoria Eshun <vee4@cornell.edu>
2023-02-09 09:41:09 -0500

    added render template

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
+
 from flask import *
 from whitenoise import WhiteNoise
 
-
 app = Flask(__name__)
-app.wsgi_app = WhiteNoise(app.wsgi_app, root='static/', prefix='', index_file='index.htm', autorefresh=True)
-
-# @app.route('/', methods=['GET'])
-# def hello():
-#     return make_response("Hello, world!!!!!")
+app.wsgi_app = WhiteNoise(app.wsgi_app, 
+                          root='public_site/', 
+                          prefix='', 
+                          index_file="index.htm",
+                          autorefresh=True)
 
 if __name__ == "__main__":
     app.run(threaded=True, port=5000)
